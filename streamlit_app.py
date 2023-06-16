0a9d45b8e6b398978093d08b0e2661f9b1cd39d4
lolasnow <136793306+lolasnow@users.noreply.github.com>
2023-06-16 15:35:16 +0200

    Update streamlit_app.py
    error in textbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,10 +27,10 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit advice !')
 
 #add textbox
-fruit_choice = streamlit.text_input('What fruit would you like information about ?', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about ?', 'Kiwi')
 streamlit.write('The user entered', fruitchoice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 # normalize the json response 
