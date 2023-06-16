bcd3af3810f4d55965a07b43dc82e0b8cece5917
lolasnow <136793306+lolasnow@users.noreply.github.com>
2023-06-16 15:36:51 +0200

    Update streamlit_app.py
    Error 2 textbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit advice !')
 
 #add textbox
-fruit_choice = streamlit.text_input('What fruit would you like information about ?', 'Kiwi')
-streamlit.write('The user entered', fruitchoice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
