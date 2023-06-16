2d0b4d2fec1576ef54f154223cbe6c4f8e80adfe
lolasnow <136793306+lolasnow@users.noreply.github.com>
2023-06-16 15:41:24 +0200

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.header('Fruityvice Fruit advice !')
 
 #add textbox
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+#streamlit.write('The user entered', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
