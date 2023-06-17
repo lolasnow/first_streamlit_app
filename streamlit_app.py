7e87f9285c109c9bb723f3776c53c5e986c72e11
lolasnow <136793306+lolasnow@users.noreply.github.com>
2023-06-17 19:22:23 +0200

    Update streamlit_app.py
    remove error

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,9 +56,9 @@ def get_fruit_load_list():
     return y_cur.fetchall()
   
 #add button
-if streamlit.button('Get fruit load list');
+if streamlit.button('Get fruit load list')
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  my_data_rows = get_frui_load_list()
+  my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
 #add a fruit to the list
